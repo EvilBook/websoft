7a1c473c3179e86152724fffe0a370d519c96a3d
EvilBook <martin.zannato@yahoo.com>
2020-01-29 16:19:18 +0100

    this is the worst

--- work/so3.1/script/table.js
+++ work/so3.1/script/table.js
@@ -25,8 +25,8 @@ button.addEventListener("click", function(){
         var row = table.insertRow(i+1);
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
-        var cell1 = row.insertCell(2);
-        var cell2 = row.insertCell(3);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
         
         cell1.innerHTML = jsonInfo.Skolenheter[i].Skolenhetskod;
         cell2.innerHTML = jsonInfo.Skolenheter[i].Skolenhetsnamn;
