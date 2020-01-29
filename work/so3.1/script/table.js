9c5fad4d7b52c0509cbadeb6fa7ba26a3b8ff821
EvilBook <martin.zannato@yahoo.com>
2020-01-29 15:49:06 +0100

    please work

--- work/so3.1/script/table.js
+++ work/so3.1/script/table.js
@@ -18,9 +18,9 @@ fetch('data/skolenhet.json')
 var button= document.getElementById('loadData');
 var table= document.getElementById('table1');
 
-button.addEventListener('click', function(){
-    for(var i=0; i <jsonInfo.length; i++){
-        var object=jsonInfo[i];
+button.addEventListener("click", function(){
+    for(var i=0; i <jsonInfo.Skolenheter.length; i++){
+        var object=jsonInfo.Skolenheter[i];
         console.log(object);
     }
 });
