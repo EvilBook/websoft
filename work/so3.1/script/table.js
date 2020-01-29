8d763e5b02671bcdb832033ce9641600d07cc400
EvilBook <martin.zannato@yahoo.com>
2020-01-29 15:22:38 +0100

    please help

--- work/so3.1/script/table.js
+++ work/so3.1/script/table.js
@@ -1,6 +1,7 @@
 (function () {
 
-fetch('https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081')        .then((response) => {
+fetch('data/skolenhet.json') 
+    .then((response) => {
             return response.json();
         })
         .then((myJson) => {
